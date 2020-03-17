081186934e952dc4287a43ff6cf53cba37b6bdcb
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-03-17 01:21:43 -0300

    Delivery List

--- src/pages/Delivery/List/index.js
+++ src/pages/Delivery/List/index.js
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Input, Form } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
+import { MdAdd, MdSearch } from 'react-icons/md';
 
-import { Container } from './styles';
+import api from '~/services/api';
+import history from '~/services/history';
+import Pagination from '~/components/Pagination';
+import NewButton from '~/components/Button';
+
+import { Container, Content, ContentTable, Table, Row } from './styles';
 
 export default function ListDelivery() {
-  return <Container>ListDelivery</Container>;
+  const [deliverys, setDeliverys] = useState([]);
+  const [delivery = '', setDelivery] = useState();
+  const [page = 1, setPage] = useState();
+  const [loading = false, setLoading] = useState();
+
+  const loadDeliveries = useCallback(() => {
+    async function load() {
+      try {
+        setLoading(true);
+        const response = await api.get('deliveries', {
+          params: {
+            delivery,
+            page,
+          },
+        });
+
+        setDeliverys(response.data.students);
+        setLoading(false);
+      } catch (err) {
+        setDeliverys([]);
+        setLoading(false);
+      }
+    }
+    load();
+  }, [page, delivery]);
+
+  useEffect(() => {
+    loadDeliveries();
+  }, [page, delivery, loadDeliveries]);
+
+  async function handleSearch({ deliverySearch }) {
+    await setDelivery(deliverySearch);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <h1>Gerenciando encomendas</h1>
+        <div>
+          <NewButton
+            title="CADASTRAR"
+            loading={loading}
+            IconButton={MdAdd}
+            type="button"
+            onClick={() => {
+              history.push('/delivery/store');
+            }}
+          />
+
+          <Form onSubmit={handleSearch}>
+            <span>
+              <MdSearch size={22} color="#999999" />
+            </span>
+            <Input name="studentSearch" placeholder="Buscar aluno" />
+          </Form>
+        </div>
+      </Content>
+      <ContentTable>
+        {loading ? (
+          <Row>
+            <h1>Carregando Encomendas...</h1>
+          </Row>
+        ) : deliverys.length <= 0 ? (
+          <Row>
+            <h1>Não foi encontrado nenhuma encomenda</h1>
+          </Row>
+        ) : (
+          <Table>
+            <thead>
+              <tr>
+                <th className="colLeft">
+                  <strong>ID</strong>
+                </th>
+                <th className="colLeft">
+                  <strong>Destinatário</strong>
+                </th>
+                <th>
+                  <strong>Entregador</strong>
+                </th>
+                <th>
+                  <strong>Cidade</strong>
+                </th>
+                <th>
+                  <strong>Estado</strong>
+                </th>
+                <th>
+                  <strong>Status</strong>
+                </th>
+                <th>
+                  <strong>Ações</strong>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {deliverys.map(item => (
+                <tr key={item.id}>
+                  <td>
+                    <p>{item.}</p>
+                  </td>
+                  <td >
+                    <p>{item.}</p>
+                  </td>
+                  <td>
+                    <p>{item.}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </ContentTable>
+      <Pagination page={page} setPage={setPage} list={deliverys} />
+    </Container>
+  );
 }
--- src/pages/Delivery/List/styles.js
+++ src/pages/Delivery/List/styles.js
@@ -1,3 +1,116 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 20px auto;
+`;
+
+export const Content = styled.div`
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  div {
+    display: flex;
+  }
+  form {
+    input {
+      width: 237px;
+      height: 36px;
+      background: #ffffff;
+      border: 1px solid #dddddd;
+      box-sizing: border-box;
+      border-radius: 4px;
+      padding-left: 40px;
+      ::placeholder {
+        font-size: 14px;
+        line-height: 16px;
+        color: #999999;
+      }
+    }
+    span {
+      position: absolute;
+      margin-left: 10px;
+      margin-top: 8px;
+    }
+  }
+`;
+
+export const ContentTable = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  width: auto;
+  max-width: 1200px;
+  padding: 25px;
+  margin: 20px;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  max-width: 1200px;
+  tr {
+    padding: 15px 0;
+  }
+  th {
+    strong {
+      font-size: 16px;
+      line-height: 19px;
+      color: #444444;
+    }
+  }
+  tr:hover {
+    border-bottom: 1px solid #eeeeee;
+  }
+  td {
+    border-bottom: 1px solid #eeeeee;
+    padding: 15px 0;
+    text-align: center;
+    p {
+      font-size: 16px;
+      line-height: 20px;
+      color: #666666;
+    }
+  }
+  tr:last-child td {
+    border-bottom: 0px;
+  }
+  .colLeft {
+    text-align: left;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 0;
+  h1 {
+    font-size: 24px;
+    line-height: 20px;
+    color: #666666;
+  }
+`;
+
+export const ButtonEdit = styled.button`
+  font-size: 15px;
+  line-height: 18px;
+  text-align: right;
+  color: #4d85ee;
+  border: 0;
+  background: #fff;
+  padding: 0;
+  margin-right: 15px;
+  float: right;
+`;
+
+export const ButtonDelete = styled.button`
+  font-size: 15px;
+  line-height: 18px;
+  text-align: right;
+  color: #de3b3b;
+  border: 0;
+  background: #fff;
+  padding: 0;
+  margin-right: 15px;
+  float: left;
+`;
